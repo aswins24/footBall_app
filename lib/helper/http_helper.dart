0ddd4067d547547fe288e540d5dcfbc5b9669356
Aswin-SasiKumar <aswin@ileafsolutions.net>
2022-02-02 09:05:04 +0530

    Fixed a bug in http_helper.dart

--- lib/helper/http_helper.dart
+++ lib/helper/http_helper.dart
@@ -12,7 +12,7 @@ class HttpHelper {
     Uri uri = params != null ? Uri.parse(baseUrl + params) : Uri.parse(baseUrl);
     try {
       final response = await http.get(uri, headers: {'X-Auth-Token': token});
-      _returnResponse(response);
+     return _returnResponse(response);
     } on SocketException {
       throw Failure('No internet connection');
     } on HttpException catch (e) {
